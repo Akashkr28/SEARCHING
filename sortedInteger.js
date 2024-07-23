ea27519f908167eeb160b123ed55a186c8a05709
Akashkr28 <email.akash2010@gmail.com>
2024-07-23 21:29:21 +0530

    code added

---
+++ sortedInteger.js
@@ -0,0 +1,12 @@
+function sortedInteger (arr x) {
+    let lo = 0;
+    let hi = n - 1;
+
+    while(lo <= ho){
+        let mid = Math.floor((lo + hi)/2);
+        if(arr[mid] <= x) {
+            
+        }  
+    } 
+
+}
